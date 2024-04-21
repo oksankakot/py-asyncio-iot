82022bc14cfe5cf7fbf5f132fdc85ac3ff8e95ba
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-04-21 13:34:45 +0300

    Fixed according to comments

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import asyncio
 import time
-from typing import Any, Awaitable
+from typing import Any, Awaitable, Tuple
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
@@ -12,51 +12,45 @@ async def run_sequence(*functions: Awaitable[Any]) -> None:
         await function
 
 
-async def run_parallel(*functions: Awaitable[Any]) -> None:
-    await asyncio.gather(*functions)
+async def run_parallel(*functions: Awaitable[Any]) -> Tuple[Any]:
+    return await asyncio.gather(*functions)
 
 
 async def main() -> None:
-    # create an IOT service
     service = IOTService()
 
-    # create and register a few devices
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
 
-    hue_light_id, speaker_id, toilet_id = await asyncio.gather(
+    results = await run_parallel(
         service.register_device(hue_light),
         service.register_device(speaker),
         service.register_device(toilet)
     )
 
-    # create a few programs
-    async def wake_up_program() -> None:
-        await run_parallel(
-            service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
-            run_sequence(
-                service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
-                service.send_msg(Message(
-                    speaker_id,
-                    MessageType.PLAY_SONG,
-                    "Rick Astley - Never Gonna Give You Up"
-                )),
-            ),
-        )
+    hue_light_id, speaker_id, toilet_id = results[0], results[1], results[2]
 
-    async def sleep_program() -> None:
-        await run_parallel(
-            service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
-            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
-            run_sequence(
-                service.send_msg(Message(toilet_id, MessageType.FLUSH)),
-                service.send_msg(Message(toilet_id, MessageType.CLEAN)),
-            ),
-        )
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+        run_sequence(
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
+            service.send_msg(Message(
+                speaker_id,
+                MessageType.PLAY_SONG,
+                "Rick Astley - Never Gonna Give You Up"
+            )),
+        ),
+    )
 
-    await wake_up_program()
-    await sleep_program()
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+        run_sequence(
+            service.send_msg(Message(toilet_id, MessageType.FLUSH)),
+            service.send_msg(Message(toilet_id, MessageType.CLEAN)),
+        ),
+    )
 
 
 if __name__ == "__main__":
